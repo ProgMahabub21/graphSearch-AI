1a2e0b61067c8bd65019aca5a7b219cb2e3b70a3
ProMahabub20 <mahabub.islam.aiub@gmail.com>
2021-06-22 00:06:02 +0600

    New problem submitted

---
+++ Lec 2 (Variable)/Example 1/variable1.py
@@ -0,0 +1,9 @@
+price = 10              #int type of variable
+rating = 4.9            #float type of variable
+name = 'Mahabub'        #String variable
+is_published = True     #Boolean variable
+
+print(price)
+print(rating)
+print(name)
+print(is_published)
